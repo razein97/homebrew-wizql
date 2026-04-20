ddb0e5257facc681455479f0b6121aee899a1c6d
Rohit Singh <27754873+razein97@users.noreply.github.com>
2026-04-20 10:49:52 +0530

    Update wizql.rb

--- Casks/wizql.rb
+++ Casks/wizql.rb
@@ -5,7 +5,7 @@ cask "wizql" do
   sha256 arm:   "272e7196dd1666f7a1a6568b2efb7f817a95bf96e773f0eadbf09f0cf6a9288b",
          intel: "64fdead9b6dd43c20880252e9a7ad41ec82cf8b0dee0faef1515cf868f09e3c0"
 
-  url "https://github.com/razein97/Wizql-Issue-Tracker/releases/download/v#{version}/WizQl_#{version}_#{arch}.dmg",
+  url "https://github.com/razein97/Wizql-Issue-Tracker/releases/download/#{version}/WizQl_#{version}_#{arch}.dmg",
     verified: "github.com/razein97/Wizql-Issue-Tracker"
   name "WizQl"
   desc "A simple cross platform database client"
